7dee67134a5ba5222216bbc1338c9ba1a20cd9ed
aafidev <49574294+aafidev@users.noreply.github.com>
2024-02-16 21:14:51 +1100

    Create config-EDITME.py

---
+++ config-EDITME.py
@@ -0,0 +1,9 @@
+import os
+from dotenv.main import load_dotenv
+
+load_dotenv()
+
+# Discord config
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "OTg2NzQ3NDkxNjQ5MjI0NzA0.GIl9uK.m1CoVYhO7mcRFuDakeBbhUhXgwS16YTZO0YJH8")
+BOT_PREFIX = ">>"
+DISCORD_CLIENT_ID = 986747491649224704
