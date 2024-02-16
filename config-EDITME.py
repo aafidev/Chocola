82722e64a76d0d6009163876473def57aa187ca7
aafidev <49574294+aafidev@users.noreply.github.com>
2024-02-16 22:10:22 +1100

    Update config-EDITME.py

--- config-EDITME.py
+++ config-EDITME.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from dotenv.main import load_dotenv
 
@@ -7,14 +6,4 @@ load_dotenv()
 # Discord config
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "DISCORD_TOKEN_HERE")
 BOT_PREFIX = ">>"
-=======
-import os
-from dotenv.main import load_dotenv
-
-load_dotenv()
-
-# Discord config
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "DISCORD_TOKEN_HERE")
-BOT_PREFIX = ">>"
->>>>>>> origin/main
-DISCORD_CLIENT_ID = CLIENT_ID_HERE
+DISCORD_CLIENT_ID = 'CLIENT_ID_HERE'
