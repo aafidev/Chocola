f5255c705cb8ccee175ac21a8e553a73947c078a
aafidev <49574294+aafidev@users.noreply.github.com>
2024-02-16 21:15:13 +1100

    Update config-EDITME.py

--- config-EDITME.py
+++ config-EDITME.py
@@ -4,6 +4,6 @@ from dotenv.main import load_dotenv
 load_dotenv()
 
 # Discord config
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "OTg2NzQ3NDkxNjQ5MjI0NzA0.GIl9uK.m1CoVYhO7mcRFuDakeBbhUhXgwS16YTZO0YJH8")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "editme")
 BOT_PREFIX = ">>"
 DISCORD_CLIENT_ID = 986747491649224704
