7d4027f3357e26d759e0b84a5304a194b4f27532
nopeless <38830903+nopeless@users.noreply.github.com>
2024-02-16 20:35:27 -0600

    feat: add mode env var for dev

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv.main import load_dotenv
 load_dotenv()
 
 # Discord config
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "DISCORD_TOKEN_HERE")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+MODE = os.getenv("MODE", "")
 BOT_PREFIX = ">>"
-DISCORD_CLIENT_ID = 'CLIENT_ID_HERE'
+DISCORD_CLIENT_ID = "CLIENT_ID_HERE"
